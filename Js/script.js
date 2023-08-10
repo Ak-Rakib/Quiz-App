5ffdb92ec653cccff0a4234bacf7cf3031de28a9
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-08-10 10:13:16 +0600

    added array of object data about quiz

---
+++ Js/script.js
@@ -0,0 +1,103 @@
+const quizData = [
+    {
+      question: "What does JSX stand for in React?",
+      options: [
+        "a) JavaScript XML",
+        "b) Java Serialized XML",
+        "c) JSON XML",
+        "d) JavaScript XSL"
+      ],
+      correctAnswer: "a) JavaScript XML"
+    },
+    {
+      question: "Which method is used to change the state of a component in React?",
+      options: [
+        "a) setState()",
+        "b) changeState()",
+        "c) updateState()",
+        "d) modifyState()"
+      ],
+      correctAnswer: "a) setState()"
+    },
+    {
+      question: "What is the virtual DOM in React?",
+      options: [
+        "a) A representation of the actual DOM in memory",
+        "b) An alternative to HTML",
+        "c) A way to style components",
+        "d) A type of conditional statement"
+      ],
+      correctAnswer: "a) A representation of the actual DOM in memory"
+    },
+    {
+      question: "Which lifecycle method is called after a component's render method?",
+      options: [
+        "a) componentDidUpdate",
+        "b) componentWillUpdate",
+        "c) componentWillRender",
+        "d) componentRendered"
+      ],
+      correctAnswer: "a) componentDidUpdate"
+    },
+    {
+      question: "In React, props are...",
+      options: [
+        "a) immutable",
+        "b) mutable",
+        "c) local variables",
+        "d) global variables"
+      ],
+      correctAnswer: "a) immutable"
+    },
+    {
+      question: "What is the purpose of useRef hook in React?",
+      options: [
+        "a) To create a reference to a DOM element",
+        "b) To fetch data from an API",
+        "c) To update the component's state",
+        "d) To handle asynchronous operations"
+      ],
+      correctAnswer: "a) To create a reference to a DOM element"
+    },
+    {
+      question: "Which hook is used for performing side effects in functional components?",
+      options: [
+        "a) useEffect",
+        "b) useSideEffect",
+        "c) useLifecycle",
+        "d) useAction"
+      ],
+      correctAnswer: "a) useEffect"
+    },
+    {
+      question: "What is the main purpose of the key prop in React when rendering lists?",
+      options: [
+        "a) To uniquely identify elements and improve performance",
+        "b) To add styling to list items",
+        "c) To provide additional data to child components",
+        "d) To reorder the list items"
+      ],
+      correctAnswer: "a) To uniquely identify elements and improve performance"
+    },
+    {
+      question: "Which component lifecycle method is invoked right before a component is removed from the DOM?",
+      options: [
+        "a) componentWillUnmount",
+        "b) componentDidUnmount",
+        "c) componentRemoved",
+        "d) componentWillRemove"
+      ],
+      correctAnswer: "a) componentWillUnmount"
+    },
+    {
+      question: "What does React Router do?",
+      options: [
+        "a) Enables navigation and routing in a single-page React application",
+        "b) Optimizes the performance of React components",
+        "c) Manages global state in a React application",
+        "d) Handles authentication and user authorization"
+      ],
+      correctAnswer: "a) Enables navigation and routing in a single-page React application"
+    }
+  ];
+  
