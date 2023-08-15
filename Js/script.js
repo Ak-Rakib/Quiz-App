2859ede72b08412b5262f8374be1f200595398fb
Ak-Rakib <rkrakibsaimon@gmail.com>
2023-08-15 12:06:56 +0600

    set showing quiz result

--- Js/script.js
+++ Js/script.js
@@ -8,7 +8,7 @@ const quizData = [
         b: "Java Serialized XML",
         c: "JSON XML",
         d: "JavaScript XSL",
-        correctAnswer: "a) JavaScript XML"
+        correctAnswer: "a"
     },
     {
         question: "Which method is used to change the state of a component in React?",
@@ -17,7 +17,7 @@ const quizData = [
         b: "changeState()",
         c: "updateState()",
         d: "modifyState()",
-        correctAnswer: "a) setState()"
+        correctAnswer: "a"
     },
     {
         question: "What is the virtual DOM in React?",
@@ -26,7 +26,7 @@ const quizData = [
         b: " An alternative to HTML",
         c: " A way to style components",
         d: " A type of conditional statement",
-        correctAnswer: "a) A representation of the actual DOM in memory"
+        correctAnswer: "a"
     },
     {
         question: "Which lifecycle method is called after a component's render method?",
@@ -35,7 +35,7 @@ const quizData = [
         b: "componentWillUpdate",
         c: "componentWillRender",
         d: "componentRendered",
-        correctAnswer: "a) componentDidUpdate"
+        correctAnswer: "a"
     },
     {
         question: "In React, props are...",
@@ -44,7 +44,7 @@ const quizData = [
         b: "mutable",
         c: "local variables",
         d: "global variables",
-        correctAnswer: "a) immutable"
+        correctAnswer: "a"
     },
     {
         question: "What is the purpose of useRef hook in React?",
@@ -52,7 +52,7 @@ const quizData = [
         b: "b) To fetch data from an API",
         c: "c) To update the component's state",
         d: "d) To handle asynchronous operations",
-        correctAnswer: "a) To create a reference to a DOM element"
+        correctAnswer: "a"
     },
     {
         question: "Which hook is used for performing side effects in functional components?",
@@ -60,7 +60,7 @@ const quizData = [
         b: "useSideEffect",
         c: "useLifecycle",
         d: "useAction",
-        correctAnswer: "a) useEffect"
+        correctAnswer: "a"
     },
     {
         question: "What is the main purpose of the key prop in React when rendering lists?",
@@ -70,7 +70,7 @@ const quizData = [
         c: "To provide additional data to child components",
         d: "To reorder the list items",
 
-        correctAnswer: "a) To uniquely identify elements and improve performance"
+        correctAnswer: "a"
     },
     {
         question: "Which component lifecycle method is invoked right before a component is removed from the DOM?",
@@ -81,7 +81,7 @@ const quizData = [
         d: "componentWillRemove",
 
 
-        correctAnswer: "a) componentWillUnmount"
+        correctAnswer: "a"
     },
     {
         question: "What does React Router do?",
@@ -91,40 +91,82 @@ const quizData = [
         c: "Manages global state in a React application",
         d: "Handles authentication and user authorization",
 
-        correctAnswer: "a) Enables navigation and routing in a single-page React application"
+        correctAnswer: "a"
     }
 ];
 
-// -------- Operation
-const quizQuestion = document.getElementById("question");
 
+
+// -------- Get Id and class from Html elements
+const resultText = document.getElementById("quiz");
+const answersEl = document.querySelectorAll(".answer");
+const quizQuestion = document.getElementById("question");
 const a_option = document.getElementById("a_option");
 const b_option = document.getElementById("b_option");
 const c_option = document.getElementById("c_option");
 const d_option = document.getElementById("d_option");
 const quizSubmit = document.getElementById("submit");
 
-let currentQuiz = 0;
 
+// -------- Get random variable and set value 0
+let currentQuiz = 0;
+let score = 0;
+
+
+loadQuiz();
 
 
 function loadQuiz() {
+    deSelected();
     const currentQuizData = quizData[currentQuiz]
-
     quizQuestion.innerText = currentQuizData.question;
     a_option.innerText = currentQuizData.a;
     b_option.innerText = currentQuizData.b;
     c_option.innerText = currentQuizData.c;
     d_option.innerText = currentQuizData.d;
-    // currentQuiz++;
-   
-
 }
 
 
+
+function optionSelected() {
+    let answerCk = undefined;
+    answersEl.forEach((answer) => {
+        // console.log(answer.value)
+        // console.log(answer.checked);
+        if (answer.checked) {
+            answerCk = answer.id
+        }
+    })
+    return answerCk;
+}
+
+
+function deSelected(){
+    answersEl.forEach((answer) => {
+        answer.checked = false;
+    })
+}
+
+
+
 quizSubmit.addEventListener("click", () => {
-    currentQuiz++;
-    loadQuiz();
+    // ------- Check see the answer
+    const answer = optionSelected();
+    console.log(answer);
+
+    if (answer) {
+        if(answer === quizData[currentQuiz].correctAnswer){
+            score++;
+        }
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        }
+        else {
+            // TO:DO -> SHOWING RESULT
+            resultText.innerHTML = `<h2>Your Score is ${score}/${quizData.length}</h2>`
+        }
+    }
 });
 
 loadQuiz();
